e5f5b5f5d6ec350ab916506f2798db92a08eee28
ckriss <bencckriss@gmail.com>
2023-06-16 20:35:51 +0200

    102-print_comb5.c fix 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 		for (j = i; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
-            		putchar((i % 10) + '0');
-            		putchar(' ');
-            		putchar((j / 10) + '0');
-            		putchar((j % 10) + '0');
-		if (i < 99 && j < 99)
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+		if (i != 99 || j != 99)
 			{
 			putchar(',');
 			putchar(' ');
