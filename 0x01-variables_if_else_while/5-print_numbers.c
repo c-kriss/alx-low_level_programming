084f5c847b09cc6359d5dad7f7387e95f5c2f6c9
ckriss <bencckriss@gmail.com>
2023-06-16 17:36:38 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - output numbers (0-9)
+ *
+ * Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0;i<10;i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
