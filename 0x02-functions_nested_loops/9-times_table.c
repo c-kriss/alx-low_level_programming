5854d42757d3ae770ab8e18966b07f6a6dadc46d
ckriss <bencckriss@gmail.com>
2023-06-20 16:12:17 +0200

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,25 +15,27 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			prod = (i * j);
-			if (i == 0 && j == 0)
+			if (j == 0)
 			{
-				prod = 0;
 				_putchar(prod + '0');
 			}
 			else
 			{
 				if (prod < 10)
 				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(prod + '0');
 				}
 				else
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
 				}
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
