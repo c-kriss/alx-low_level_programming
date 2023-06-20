2a582857da09bf6f0d7ffc8a0502441ed3f47dad
ckriss <bencckriss@gmail.com>
2023-06-20 09:57:15 +0200

    0-putchar.c Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - Print _putchar
- * Return: Always 0 (Success)
+ * Author : Ckriss
+ * Return: 0
  */
 int main(void)
 {
-	write(1, "_putchar\n", 9);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
