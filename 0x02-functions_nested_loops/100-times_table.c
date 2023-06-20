73951d40646c2a20667e5ad6434fed7c3c171b20
ckriss <bencckriss@gmail.com>
2023-06-20 22:49:45 +0200

    34343

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,7 +29,9 @@ void print_times_table(int n)
 			if (prod < 10)
 				_putchar(' ');
 			if (prod >= 100)
+			{
 				_putchar((prod / 100) + '0');
+			}
 			if (prod >= 10)
 				_putchar(((prod / 10) % 10) + '0');
 				_putchar((prod % 10) + '0');
