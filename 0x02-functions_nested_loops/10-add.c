4df9cbccdf9aa5e0f32d8b656cc73e64e93e3490
ckriss <bencckriss@gmail.com>
2023-06-20 16:50:14 +0200

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - for alphabets
+ * @ab: print two numbers
+ *
+ * Description: It print natural numbers
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+	return (sum);
+}
