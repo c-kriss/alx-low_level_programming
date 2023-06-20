93580a387dea96c32901cf0eef4bc5abe963a5e7
ckriss <bencckriss@gmail.com>
2023-06-20 22:40:53 +0200

    100-times_table final

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,37 +11,31 @@ void print_times_table(int n)
 {
     int i, j, prod;
 
-    if (n > 0 && n < 16)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                prod = (i * j);
-
-                if (j == 0)
-                    _putchar(prod + '0');
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
-
-                    if (prod < 100)
-                        _putchar(' ');
-
-                    if (prod < 10)
-                        _putchar(' ');
-
-                    if (prod >= 100)
-                        _putchar((prod / 100) + '0');
-
-                    if (prod >= 10)
-                        _putchar(((prod / 10) % 10) + '0');
-
-                    _putchar((prod % 10) + '0');
+	if (n >= 0 && n <= 15)
+	{
+	for (i = 0; i <= n; i++)
+	{
+	for (j = 0; j <= n; j++)
+	{
+		prod = (i * j);
+		if (j == 0)
+			_putchar(prod + '0');
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			if (prod < 100)
+				_putchar(' ');
+			if (prod < 10)
+				_putchar(' ');
+			if (prod >= 100)
+				_putchar((prod / 100) + '0');
+			if (prod >= 10)
+				_putchar(((prod / 10) % 10) + '0');
+				_putchar((prod % 10) + '0');
 		}
-            }
-            _putchar('\n');
-        }
-    }
+	}
+		_putchar('\n');
+	}
+	}
 }
