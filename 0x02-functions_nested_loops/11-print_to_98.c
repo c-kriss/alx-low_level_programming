e3411bd5a3934ada421baf15a38a3cda2957c940
ckriss <bencckriss@gmail.com>
2023-06-20 17:09:45 +0200

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - for alphabets
+ * @n: print character
+ *
+ * Description: It print natural numbers
+ * No return
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+	for (i = n; i <=98; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+	}
+	}
+	else
+	{
+		for (i = n; i >=98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	
+	print_to_98(0);
+	printf("\n");
+    	print_to_98(98);
+    	printf("\n");
+
+	print_to_98(111);
+    	printf("\n");
+
+	print_to_98(81);
+    	printf("\n");
+
+	print_to_98(-10);
+    	printf("\n");
+
+	return (0);
+}
