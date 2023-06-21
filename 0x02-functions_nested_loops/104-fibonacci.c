e571944687ea1fb08aefeded91e39107b029f9ef
ckriss <bencckriss@gmail.com>
2023-06-21 02:04:48 +0200

    104-fibonacci.c 4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
-	/**
-	 * main - fibonacci <3
-	 *
-	 * Purpose - no hardcode
-	 *
-	 * Return:  (Success)
-	 */
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return: 0 (Success)
+ */
 
 
 	int main(void)
