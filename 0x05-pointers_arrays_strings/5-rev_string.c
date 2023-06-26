0d9809ee1d50eb1d013ec8ee1b887355df85fe0d
ckriss <bencckriss@gmail.com>
2023-06-26 23:03:11 +0200

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+/**
+ * rev_string - Print string in reverse order
+ * @s: String input pointer
+ * Return: no return
+ */
+void rev_string(char *s)
+{
+	int i;
+	int length = strlen(s);
+	int start = 0;
+	int end = length - 1;
+
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
