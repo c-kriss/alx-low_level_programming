bdec9b01e9f82d0e310cbf0121622fa8b4636e93
ckriss <bencckriss@gmail.com>
2023-06-29 16:22:28 +0200

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcat - Function pointer to concatinate 2 strings
+ * @dest: String input pointer
+ * @src: String input pointer
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
