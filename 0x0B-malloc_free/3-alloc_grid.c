ff7d99f98bc7bec0fb8b6b4bed560e6a23b613f5
c-kriss <benckriss@gmail.com>
2023-07-11 22:20:44 +0200

    3-alloc_grid.c V#

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
 
