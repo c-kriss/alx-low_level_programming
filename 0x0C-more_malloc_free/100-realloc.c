e560a584d450f21f0e1a9b388b05bc92a1e5e758
c-kriss <benckriss@gmail.com>
2023-07-14 00:28:03 +0200

    100-realloc.c V2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,12 +38,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (newadd);
 	}
 
-	if (new_size > old_size)
-	{
-		memcpy(newadd, ptr, old_size < new_size ? old_size : new_size);
-		free(ptr);
-		return (newadd);
-	}
+	memcpy(newadd, ptr, old_size < new_size ? old_size : new_size);
+	free(ptr);
 
 	return (newadd);
 }
