92a1d2c32ee2db2060bf955ecb4298844a9b1628
c-kriss <benckriss@gmail.com>
2023-07-21 01:13:12 +0200

    aser

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,19 @@
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int i;
+	unsigned int sum = 0;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(ap, int);
-
+	}
 	va_end(ap);
-
 	return (sum);
 }
