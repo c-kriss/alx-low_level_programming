40fb27980c376cb49f4b73a1b3b4b3b6b2a55a0f
c-kriss <benckriss@gmail.com>
2023-08-07 17:00:58 +0200

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,28 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Name of the text file.
- * @letters: Number of letters to read and print.
- *
- * Return: Number of bytes read and printed, or 0 on failure.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
-
-	if (filename == NULL)
-		return (0);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes_read = read(fd, buffer, letters);
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	free(buffer);
+	free(buf);
 	close(fd);
-
-	if (bytes_read == -1 || bytes_written == -1)
-		return (0);
-
-	return (bytes_written);
+	return (w);
 }
--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -50,17 +50,10 @@ void print_magic(unsigned char *e_ident)
 {
 	int index;
 
-	printf(" Magic: ");
-
+	printf(" Magic:");
 	for (index = 0; index < EI_NIDENT; index++)
-	{
-		printf("%02x", e_ident[index]);
-
-		if (index == EI_NIDENT - 1)
-			printf("\n");
-		else
-			printf(" ");
-	}
+		printf(" %02x", e_ident[index]);
+	printf("\n");
 }
 
 /**
@@ -69,21 +62,21 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf(" Class: ");
+	printf(" Class:");
 
 	switch (e_ident[EI_CLASS])
 	{
 	case ELFCLASSNONE:
-		printf("none\n");
+		printf(" none\n");
 		break;
 	case ELFCLASS32:
-		printf("ELF32\n");
+		printf(" ELF32\n");
 		break;
 	case ELFCLASS64:
-		printf("ELF64\n");
+		printf(" ELF64\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+		printf(" <unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
@@ -93,33 +86,31 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf(" Data: ");
+	printf(" Data:");
 
 	switch (e_ident[EI_DATA])
 	{
 	case ELFDATANONE:
-		printf("none\n");
+		printf(" none\n");
 		break;
 	case ELFDATA2LSB:
-		printf("2's complement, little endian\n");
+		printf(" 2's complement, little endian\n");
 		break;
 	case ELFDATA2MSB:
-		printf("2's complement, big endian\n");
+		printf(" 2's complement, big endian\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+		printf(" <unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
 
 /**
  * print_version - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
- *
  */
 void print_version(unsigned char *e_ident)
 {
-	 printf(" Version: %d",
-			  e_ident[EI_VERSION]);
+	printf(" Version: %d", e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
@@ -138,42 +129,42 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf(" OS/ABI: ");
+	printf(" OS/ABI:");
 
 	switch (e_ident[EI_OSABI])
 	{
 	case ELFOSABI_NONE:
-		printf("UNIX - System V\n");
+		printf(" UNIX - System V\n");
 		break;
 	case ELFOSABI_HPUX:
-		printf("UNIX - HP-UX\n");
+		printf(" UNIX - HP-UX\n");
 		break;
 	case ELFOSABI_NETBSD:
-		printf("UNIX - NetBSD\n");
+		printf(" UNIX - NetBSD\n");
 		break;
 	case ELFOSABI_LINUX:
-		printf("UNIX - Linux\n");
+		printf(" UNIX - Linux\n");
 		break;
 	case ELFOSABI_SOLARIS:
-		printf("UNIX - Solaris\n");
+		printf(" UNIX - Solaris\n");
 		break;
 	case ELFOSABI_IRIX:
-		printf("UNIX - IRIX\n");
+		printf(" UNIX - IRIX\n");
 		break;
 	case ELFOSABI_FREEBSD:
-		printf("UNIX - FreeBSD\n");
+		printf(" UNIX - FreeBSD\n");
 		break;
 	case ELFOSABI_TRU64:
-		printf("UNIX - TRU64\n");
+		printf(" UNIX - TRU64\n");
 		break;
 	case ELFOSABI_ARM:
-		printf("ARM\n");
+		printf(" ARM\n");
 		break;
 	case ELFOSABI_STANDALONE:
-		printf("Standalone App\n");
+		printf(" Standalone App\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
+		printf(" <unknown: %x>\n", e_ident[EI_OSABI]);
 	}
 }
 
@@ -183,8 +174,7 @@ void print_osabi(unsigned char *e_ident)
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf(" ABI Version: %d\n",
-		e_ident[EI_ABIVERSION]);
+	printf(" ABI Version: %d\n", e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -197,27 +187,27 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
-	printf(" Type: ");
+	printf(" Type:");
 
 	switch (e_type)
 	{
 	case ET_NONE:
-		printf("NONE (None)\n");
+		printf(" NONE (None)\n");
 		break;
 	case ET_REL:
-		printf("REL (Relocatable file)\n");
+		printf(" REL (Relocatable file)\n");
 		break;
 	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
+		printf(" EXEC (Executable file)\n");
 		break;
 	case ET_DYN:
-		printf("DYN (Shared object file)\n");
+		printf(" DYN (Shared object file)\n");
 		break;
 	case ET_CORE:
-		printf("CORE (Core file)\n");
+		printf(" CORE (Core file)\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_type);
+		printf(" <unknown: %x>\n", e_type);
 	}
 }
 
@@ -228,20 +218,19 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf(" Entry point address: ");
+	printf(" Entry point address:");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = ((e_entry << 8) & 0xFF00FF00) |
-			  ((e_entry >> 8) & 0xFF00FF);
+			((e_entry >> 8) & 0xFF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 
 	if (e_ident[EI_CLASS] == ELFCLASS32)
-		printf("%#x\n", (unsigned int)e_entry);
-
+		printf(" %#x\n", (unsigned int)e_entry);
 	else
-		printf("%#lx\n", e_entry);
+		printf(" %#lx\n", e_entry);
 }
 
 /**
@@ -254,8 +243,7 @@ void close_elf(int elf)
 {
 	if (close(elf) == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", elf);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", elf);
 		exit(98);
 	}
 }
